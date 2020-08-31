8546c4dae5f2fd6d264d6301cfb866dfd93c5fae
Benjamin Mishra <Benjamin.Mishra2@shell.com>
2020-08-31 14:13:29 +0530

    Added request logging middleware

--- 03-restapi-with-node-express/moviecataloguebackend/app.js
+++ 03-restapi-with-node-express/moviecataloguebackend/app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const movieRouter = require('./movieRouter');
+const reqLoggingMw = require('./loggingMiddleware');
 
 // Middleware
+
+app.use((req,res,next)=>{
+    console.log(`${req.method}: ${req.path} @ ${new Date().toDateString()}`);
+    next();
+});
 app.use(bodyParser.json())
 app.use('/api/movies',movieRouter);
 
 
+// Routes
 app.get('/', (req, res) => {
     res.send("Hello World!!");
 });
