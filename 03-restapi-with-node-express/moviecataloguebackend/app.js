84f9adda2168a8e9a811f61a0de34fb81d1dcb05
Benjamin Mishra <Benjamin.Mishra2@shell.com>
2020-08-31 17:56:47 +0530

    Removed the import for middleware module

--- 03-restapi-with-node-express/moviecataloguebackend/app.js
+++ 03-restapi-with-node-express/moviecataloguebackend/app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const movieRouter = require('./movieRouter');
-const reqLoggingMw = require('./loggingMiddleware');
 
 // Middleware
-
 app.use((req,res,next)=>{
+    // log into console
     console.log(`${req.method}: ${req.path} @ ${new Date().toDateString()}`);
-    next();
+    next(); // pass on the request to next middlware
 });
 app.use(bodyParser.json())
 app.use('/api/movies',movieRouter);
